b4a268f9ec19a35931e86e44e7b6ab3eedcd3b37
ZhiChen <zilingqishi@gmail.com>
2015-03-10 06:54:30 +0800

    tiny modification

--- src/GoogleCluster/python/helper/parse_log.py
+++ src/GoogleCluster/python/helper/parse_log.py
@@ -1,52 +1,58 @@
 __author__ = 'ubuntu'
 
+import os
 import sys
 
-INPUT_FILE = "/home/ubuntu/sar.log"
-OUTPUT_FILE = "/home/ubuntu/out.csv"
+# INPUT_FILE = "/home/chenzhi/log/1/172.18.9.42-23:11:53.log"
+# OUTPUT_FILE = "/home/chenzhi/out.csv"
+INPUT_PATH = "/home/chenzhi/log/32/"
 CYCLE = 8
-CPU_INDEX = [1, 3]
+CPU_INDEX = [1, 8]
 MEM_INDEX = [3, 4]
 DISK_INDEX = [5, 5] # three value [5, 5] [6, 5] [7, 5]
-CPU_METRICS = []
-MEM_METRICS = []
-DISK_METRICS = []
 
 if __name__ == "__main__":
-    matrix = []
-    reader = open(INPUT_FILE, 'r')
-    # ignore the first two line
-    reader.readline()
-    reader.readline()
-    print 'Begin to read...'
-    line = reader.readline()
-    while line != '':
-        if line == '\n':
-            line = reader.readline()
-            continue
-        else:
-            matrix.append(line.split())
-            line = reader.readline()
-    reader.close()
-    print "Finish to read..."
-    print 'Total length', len(matrix)
+    for file_name in sorted(os.listdir(INPUT_PATH)):
+        matrix = []
+        cpu_metrics = []
+        mem_metrics = []
+        disk_metrics = []
+        reader = open(os.path.join(INPUT_PATH, file_name), 'r')
+        # ignore the first two line
+        reader.readline()
+        reader.readline()
+        print 'Begin to read...'
+        line = reader.readline()
+        while line != '':
+            if line == '\n':
+                line = reader.readline()
+                continue
+            else:
+                matrix.append(line.split())
+                line = reader.readline()
+        reader.close()
+        print "Finish to read..."
+        print 'Total length', len(matrix)
 
-    if len(matrix) % CYCLE != 0:
-        print "Can't be divide exactly!!!"
-        sys.exit()
-    for i in range(len(matrix)/CYCLE):
-        CPU_METRICS.append(matrix[CPU_INDEX[0] + i * CYCLE][CPU_INDEX[1]])
-        MEM_METRICS.append(matrix[MEM_INDEX[0] + i * CYCLE][MEM_INDEX[1]])
-        DISK_METRICS.append(str((float(matrix[DISK_INDEX[0] + i * CYCLE][DISK_INDEX[1]]) +
-                            float(matrix[DISK_INDEX[0] + i * CYCLE + 1][DISK_INDEX[1]]) +
-                            float(matrix[DISK_INDEX[0] + i * CYCLE + 2][DISK_INDEX[1]])) / 3))
-    # print CPU_METRICS
-    # print MEM_METRICS
-    # print DISK_METRICS
-    print 'Begin to output...'
-    output = open(OUTPUT_FILE, 'w')
-    output.write(",".join(CPU_METRICS) + '\n')
-    output.write(",".join(MEM_METRICS) + '\n')
-    output.write(",".join(DISK_METRICS) + '\n')
-    output.close()
-
+        if len(matrix) % CYCLE != 0:
+            print "Can't be divide exactly!!!"
+            sys.exit()
+        for i in range(len(matrix)/CYCLE - 1): # minus 1 to omit last cycle, because the last is summary
+            cpu_metrics.append(str(100 - float(matrix[CPU_INDEX[0] + i * CYCLE][CPU_INDEX[1]])))
+            mem_metrics.append(matrix[MEM_INDEX[0] + i * CYCLE][MEM_INDEX[1]])
+            disk_metrics.append(str((float(matrix[DISK_INDEX[0] + i * CYCLE][DISK_INDEX[1]]) +
+                                float(matrix[DISK_INDEX[0] + i * CYCLE + 1][DISK_INDEX[1]]) +
+                                float(matrix[DISK_INDEX[0] + i * CYCLE + 2][DISK_INDEX[1]])) / 3))
+        # print cpu_metrics
+        # print mem_metrics
+        # print disk_metrics
+        print 'Begin to output...'
+        # output = open(OUTPUT_FILE, 'w')
+        # output.write(",".join(cpu_metrics) + '\n')
+        # output.write(",".join(mem_metrics) + '\n')
+        # output.write(",".join(disk_metrics) + '\n')
+        # output.close()
+        print file_name
+        print("\t".join(cpu_metrics) + '\n')
+        # print(",".join(mem_metrics) + '\n')
+        # print(",".join(disk_metrics) + '\n')
--- src/GoogleCluster/python/test/testMySQL.py
+++ src/GoogleCluster/python/test/testMySQL.py
@@ -7,35 +7,46 @@ import csv
 import os
 import traceback
 
-INPUT_PATH = "/home/ubuntu/Documents/task_events_by_machine/"
-INPUT_FILES = os.listdir(INPUT_PATH)
-INPUT_FILE = INPUT_FILES[0]
 
+def test_insert():
+    row = ["3600373469", "", "515042969", "11", "369730124", "1", "/fk1fVcVxZ6iM6gHZzqbIyq56m5zrmHfpdcZ/zzkq4c=",
+           "2", "0", "0.01562", "0.01553", "0.0002155", "0"]
+    db = MySQLdb.connect(host="localhost", # your host, usually localhost
+                         user="root", # your username
+                          passwd="123", # your password
+                          db="googlecluster") # name of the data base
 
-# read from csv file
-inFile = open(INPUT_PATH + INPUT_FILE, 'r')
-reader = csv.reader(inFile, delimiter=',', quoting=csv.QUOTE_NONE)
-# print dir(reader)
-row = reader.next()
-inFile.close()
-print row
-db = MySQLdb.connect(host="localhost", # your host, usually localhost
-                     user="root", # your username
-                      passwd="123", # your password
-                      db="GoogleCluster") # name of the data base
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor()
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cur = db.cursor()
+    # Use all the SQL you like
+    query = '''insert into task_events values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+    try:
+        cur.execute(query, row)
+        db.commit()
+    except Exception, e:
+        print 'fail to insert value' + str(row)
+        print traceback.format_exc()
+        db.rollback()
+    print 'Insert Success.'
+    db.close()
 
-# Use all the SQL you like
-query = '''insert into task_events values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
-try:
-    cur.execute(query, row)
-    db.commit()
-except Exception, e:
-    print 'fail to insert value' + str(row)
-    print traceback.format_exc()
-    db.rollback()
+def test_connection():
+    try:
+        db = MySQLdb.connect(host="localhost", # your host, usually localhost
+                             user="root", # your username
+                              passwd="123", # your password
+                              db="googlecluster") # name of the data base
 
-db.close()
+        # you must create a Cursor object. It will let
+        #  you execute all the queries you need
+    except Exception as e:
+        print 'Fail to connect'
+        print traceback.format_exc()
+
+    print 'Connection Success.'
+    db.close()
+
+if __name__ == "__main__":
+    test_insert()
